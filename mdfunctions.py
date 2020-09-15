a4293ffa46d0b22c80c21cd61ea7d80271736834
SlamminU <39348477+SlamminU@users.noreply.github.com>
2020-09-15 01:43:50 -0700

    Add files via upload

---
+++ mdfunctions.py
@@ -0,0 +1,18 @@
+import math
+# Function to produce distance between two list points
+def dist(a,b):
+    return math.sqrt((a[0]-b[0])**2+(a[1]-b[1])**2+(a[2]-b[2])**2)
+    ## Insert line from lecture notes here
+
+# Function to produce square magnitude of vector
+def sqmag(a):
+    return (a[0]**2+a[1]**2+a[2]**2)
+
+# Function to return unit vector pointing between two points
+def vector(a,b):
+    ## Insert lines from lecture notes here
+    r=dist(a,b)
+    x=(a[0]-b[0])/r
+    y=(a[1]-b[1])/r
+    z=(a[2]-b[2])/r 
+    return [x,y,z]
